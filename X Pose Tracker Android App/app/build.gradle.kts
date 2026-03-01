a982496d25c397a6ece228271306551f88e5087c
RifqiMuafa20 <muhammadrifqi20001@gmail.com>
2026-03-01 16:24:04 +0800

    app ready to publish

--- X Pose Tracker Android App/app/build.gradle.kts
+++ X Pose Tracker Android App/app/build.gradle.kts
@@ -12,9 +12,9 @@ android {
     defaultConfig {
         applicationId = "com.rifqidev.x_posetracker"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
